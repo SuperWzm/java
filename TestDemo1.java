eada4c4890f8238af2a4e615b4e0a89fd22b74f4
SuperWzm <1287582532@qq.com>
2019-04-11 11:18:43 +0800

    a short

---
+++ TestDemo1.java
@@ -0,0 +1,25 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: wang
+ * Date: 2019-04-04
+ * Time: 20:15
+ **/
+public class TestDemo1 /*{
+ /  public static void main(String[] args){
+        int[] array={1,3,1,5,5,3,8};
+        int tmp =0;
+        for(int i= 0;i<array.length;i++){
+            tmp ^=array[i];
+        }
+        System.out.println(tmp);
+
+    }
+}*/
+{
+    public static void main(String[] args){
+        int x=1;
+        float y=2;
+        System.out.println(x/y);
+    }
+}
