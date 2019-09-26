efba088006b78c07bcd95e2fdb4843895e1a8d3c
SuperWzm <1287582532@qq.com>
2019-09-26 18:51:28 +0800

    exercise

---
+++ Test3.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: wang
+ * Date: 2019-09-26
+ * Time: 18:26
+ **/
+public class Test3 {
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "abc";
+        System.out.println(s1 == s2);
+    }
+}    
+    
