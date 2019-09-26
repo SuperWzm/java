e22537adb348c182c0c14bd201c7c46acf0cea0e
SuperWzm <1287582532@qq.com>
2019-09-26 18:50:54 +0800

    EXERCISE

---
+++ Test2.java
@@ -0,0 +1,20 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: wang
+ * Date: 2019-09-26
+ * Time: 18:05
+ **/
+public class Test2 {
+    private int count;
+
+    public static void main(String[] args) {
+        Test2 test2 = new Test2(88);
+        System.out.println(test2.count);
+    }
+
+    Test2(int a){
+        count = a;
+    }
+}    
+    
