a86596a5459d4914e47c0f48f6237300d558964a
SuperWzm <1287582532@qq.com>
2019-09-29 22:58:12 +0800

    switch

---
+++ TestSwitch.java
@@ -0,0 +1,23 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: wang
+ * Date: 2019-09-29
+ * Time: 21:44
+ **/
+public class TestSwitch {
+    public static void main(String[] args) {
+        int ch = 2;
+        switch (ch){
+            case 1:
+                System.out.println("内容是1");
+            case 2:
+                System.out.println("内容是2");
+            case 3:
+                System.out.println("内容是3");
+                default:
+                    System.out.println("默认值");
+        }
+    }
+}    
+    
