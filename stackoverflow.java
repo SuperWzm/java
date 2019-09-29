42f26fade3832e9a8fa75c685e6df512fdf1850e
SuperWzm <1287582532@qq.com>
2019-09-29 21:30:14 +0800

    stack over flow

---
+++ stackoverflow.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: wang
+ * Date: 2019-09-29
+ * Time: 21:02
+ **/
+public class TestDemo1 {
+
+    public static void main(String[] args) {
+        fun(10);
+    }
+
+    public static void fun(int n){
+        fun(n-1);
+    }
+}    
+    
